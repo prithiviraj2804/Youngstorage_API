90679f3d6d40632a0199d1e7ce4b5bc48b025cfb
prithiviraj2804 <prithivi2804raj@gmail.com>
2023-06-30 17:49:02 +0000

    Latest

--- app/routers/root.py
+++ app/routers/root.py
@@ -5,5 +5,5 @@ router = APIRouter()
 
 @router.get("/")
 def root():
-    # query = db.user.insert_one({"name":"test","status":True}).inserted_id
+    query = db.user.insert_one({"name":"test","status":True}).inserted_id
     return {"message":"Welcome To YoungStorage API Service","status":True}
--- app/routers/signup.py
+++ app/routers/signup.py
@@ -5,25 +5,28 @@ from ..database import db
 router = APIRouter()
 
 password_regex = "((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})"
+phone_regex = "^[0-9]{10}$"
 
 class Signup(BaseModel):
-    email : EmailStr
+    email : EmailStr = Field(...,)
     password : str = Field(..., regex=password_regex)
-    phone : int
+    phone : str = Field(..., regex=phone_regex)
 
-    # @validator("email")
-    # def email_validation(cls,email):
-    #     verify = email.split("@",-1)
-    #     allowed = ["gmail.com",'hotmail.com',"outlook.com","icloud.com"]
-    #     if verify[1] not in allowed:
-    #         return {"message":"Please Enter Valid Email Domain","status":False}
+    @validator("email")
+    def email_validation(cls,email):
+        verify = email.split("@",-1)
+        allowed = ["gmail.com",'hotmail.com',"outlook.com","icloud.com","protonmail.com","live.com"]
+        if verify[1] not in allowed:
+            raise {"message":"Please Enter Valid Email Domain","status":False}
+        return email,verify
         
-
 @router.post("/signup")
-async def signup(data : Signup):
+def signup(data:Signup):
     try:
-        if data:
-            return {"message":data,"status":True}
-        
+        if db.user.find_one({"email":data.email}) or db.user.find_one({"phone":data.phone}):
+            return {"message":"email or phone number already exists","status":False}
+        else:
+            db.user.insert_many(data.dict())
+            return {"message":"User Signed Up Successfully","status":True}
     except Exception as e:
         return {"message":str(e),"status":False}
