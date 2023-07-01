dbc2ebcca9b0457c23679ab57fc364d2e07b9936
nocturnalplay <nocturnalplay0@gmail.com>
2023-07-01 13:00:14 +0530

    updated

--- app/routers/signup.py
+++ app/routers/signup.py
@@ -1,32 +1,53 @@
 from fastapi import APIRouter
-from pydantic import BaseModel,EmailStr,Field,validator
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel, EmailStr, Field, validator
 from ..database import db
+import re
 
 router = APIRouter()
 
-password_regex = "((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})"
-phone_regex = "^[0-9]{10}$"
+password_regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$"
+phone_regex = r"^[0-9]{10}$"
+
 
 class Signup(BaseModel):
-    email : EmailStr = Field(...,)
-    password : str = Field(..., regex=password_regex)
-    phone : str = Field(..., regex=phone_regex)
+    email: EmailStr
+    password: str
+    phone: str
 
     @validator("email")
-    def email_validation(cls,email):
-        verify = email.split("@",-1)
-        allowed = ["gmail.com",'hotmail.com',"outlook.com","icloud.com","protonmail.com","live.com"]
+    def email_validation(cls, email):
+        verify = email.split("@")
+        allowed = ["gmail.com", 'hotmail.com', "outlook.com",
+                   "icloud.com", "protonmail.com", "live.com"]
         if verify[1] not in allowed:
-            raise {"message":"Please Enter Valid Email Domain","status":False}
-        return email,verify
-        
+            raise ValueError("Please Enter Valid Email Domain")
+        return email
+
+    @validator("password")
+    def password_validation(cls, password):
+        if not re.match(password_regex, password):
+            raise ValueError(
+                "Please enter a password that meets the required pattern")
+        return password
+
+    @validator("phone")
+    def phone_validation(cls, phone):
+        if not re.match(phone_regex, phone):
+            raise ValueError("Please enter a valid phone number")
+        return phone
+
+
 @router.post("/signup")
-def signup(data:Signup):
+def signup(data: Signup):
     try:
-        if db.user.find_one({"email":data.email}) or db.user.find_one({"phone":data.phone}):
-            return {"message":"email or phone number already exists","status":False}
+        if db.user.find_one({"email": data.email}):
+            return {"message": "email already exists", "status": False}
+        elif db.user.find_one({"phone": data.phone}):
+            return {"message": "phone number already exists", "status": False}
         else:
-            db.user.insert_many(data.dict())
-            return {"message":"User Signed Up Successfully","status":True}
+            db.user.insert_one(data.dict())
+            return {"message": "User Signed Up Successfully", "status": True}
     except Exception as e:
-        return {"message":str(e),"status":False}
+        return {"message": str(e), "status": False}
