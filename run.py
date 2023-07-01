546949066daf8841180fabb20340ee7a3288394e
nocturnalplay <nocturnalplay0@gmail.com>
2023-07-01 11:43:49 +0530

    updated

--- run.py
+++ run.py
@@ -11,7 +11,5 @@ app = FastAPI()
 app.include_router(root.router)
 app.include_router(signup.router)
 
-
-
 if __name__ == "__main__":
     uvicorn.run("run:app",host="0.0.0.0",port=8000,reload=True)
